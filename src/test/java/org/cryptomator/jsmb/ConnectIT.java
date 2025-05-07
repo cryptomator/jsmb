132cba90a026a6f2b40da91059a4ff683ac3c1ab
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2025-05-07 12:20:40 +0200

    add smbj for integration testing

---
+++ src/test/java/org/cryptomator/jsmb/ConnectIT.java
@@ -0,0 +1,63 @@
+package org.cryptomator.jsmb;
+
+import com.hierynomus.mserref.NtStatus;
+import com.hierynomus.mssmb2.SMBApiException;
+import com.hierynomus.smbj.SMBClient;
+import com.hierynomus.smbj.SmbConfig;
+import com.hierynomus.smbj.auth.AuthenticationContext;
+import com.hierynomus.smbj.connection.Connection;
+import com.hierynomus.smbj.session.Session;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ConnectIT {
+
+	private TcpServer server;
+	private SMBClient client;
+
+	@BeforeEach
+	public void setup() throws IOException {
+		server = TcpServer.start(4445);
+		SmbConfig config = SmbConfig.builder()
+				.withMultiProtocolNegotiate(true) //
+				.withEncryptData(true) //
+				.withSigningEnabled(true) //
+				.build();
+		client = new SMBClient(config);
+	}
+
+	@AfterEach
+	public void tearDown() throws IOException {
+		if (client != null) {
+			client.close();
+		}
+		if (server != null) {
+			server.close();
+		}
+	}
+
+	@Test
+	@DisplayName("Attempt to connect with invalid credentials")
+	public void connectWithInvalidCredentials() throws IOException {
+		try (Connection connection = client.connect("localhost", server.getLocalPort())) {
+			AuthenticationContext ac = new AuthenticationContext("user", "wrongPassword".toCharArray(), "DOMAIN");
+			var e = Assertions.assertThrows(SMBApiException.class, () -> connection.authenticate(ac));
+			Assertions.assertEquals(NtStatus.STATUS_LOGON_FAILURE, e.getStatus());
+		}
+	}
+
+	@Test
+	@DisplayName("Connect and disconnect with valid credentials")
+	public void connectAndDisconnect() throws IOException {
+		try (Connection connection = client.connect("localhost", server.getLocalPort())) {
+			AuthenticationContext ac = new AuthenticationContext("user", "password".toCharArray(), "DOMAIN");
+			Session session = connection.authenticate(ac);
+			Assertions.assertTrue(session.isSigningRequired());
+		}
+	}
+}
