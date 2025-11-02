1baf0ac43fa7e2055606b8b329fa46ba2eb6d475
JaniruTEC <52893617+JaniruTEC@users.noreply.github.com>
2025-11-02 17:27:28 +0100

    Replaced implementation of CMAC signing
    Prior implementation mixed javax.crypto/Bouncy Castle in ways that led to unexpected behavior such as completely ignoring the "AES128" parameter in line 101.
    
    See: https://github.com/cryptomator/jsmb/pull/2#discussion_r2424987183

--- src/main/java/org/cryptomator/jsmb/smb2/crypto/MessageSigner.java
+++ src/main/java/org/cryptomator/jsmb/smb2/crypto/MessageSigner.java
@@ -1,6 +1,8 @@
 package org.cryptomator.jsmb.smb2.crypto;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.crypto.engines.AESEngine;
+import org.bouncycastle.crypto.macs.CMac;
+import org.bouncycastle.crypto.params.KeyParameter;
 import org.cryptomator.jsmb.smb2.Command;
 import org.cryptomator.jsmb.smb2.Connection;
 import org.cryptomator.jsmb.smb2.PacketHeader;
@@ -13,7 +15,6 @@ import org.jetbrains.annotations.VisibleForTesting;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.Mac;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -36,7 +37,7 @@ public class MessageSigner {
 	private static final int NONCE_FLAG_IS_SERVER = 0b1;
 	private static final int NONCE_FLAG_IS_CANCEL = 0b10;
 
-	private static final BouncyCastleProvider bcProvider = new BouncyCastleProvider();
+	private static final int CMAC_LENGTH = 128;
 
 	public PacketHeader sign(SMB2Message message, byte[] signingKey, Connection connection) {
 		assert Objects.equals(connection.dialect, "3.1.1");
@@ -96,14 +97,13 @@ public class MessageSigner {
 
 	@VisibleForTesting
 	byte[] cmac(byte[] data, byte[] signingKeyBytes) {
-		try {
-			Mac mac = Mac.getInstance("AESCMAC", bcProvider);
-			mac.init(new SecretKeySpec(signingKeyBytes, "AES128"));
-			return mac.doFinal(data);
-		} catch (NoSuchAlgorithmException e) {
-			throw new AssertionError("AESCMAC should be provided by Bouncy Castle", e);
-		} catch (InvalidKeyException e) {
-			throw new IllegalArgumentException("Invalid key or algorithm parameter", e);
-		}
+		var mac = new CMac(AESEngine.newInstance(), CMAC_LENGTH);
+		var params = new KeyParameter(signingKeyBytes);
+		mac.init(params);
+		mac.update(data, 0, data.length);
+
+		var result = new byte[CMAC_LENGTH / 8];
+		mac.doFinal(result, 0);
+		return result;
 	}
 }
